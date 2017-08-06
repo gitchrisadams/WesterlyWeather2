136aaf6a6486bb19b0ecb0e0a3744bb0cba1f65b
Christopher Adams <chrismichaeladams@gmail.com>
2017-08-06 12:26:17 -0400

    Weather app pulling in data

--- js/script2.js
+++ js/script2.js
@@ -3,4 +3,23 @@
 (function() {
 	var url = "http://api.openweathermap.org/data/2.5/weather?q=London,England";
     var httpRequest;
+
+    // Make the api request:
+    makeRequest();
+
+    // Create and send XHR request.
+    function makeRequest() {
+
+      httpRequest = new XMLHttpRequest();
+      httpRequest.onreadystatechange = responseMethod;   // When http request is ready, call responseMethod.
+      httpRequest.open('GET', url + '&appid=' + apiKey);
+      httpRequest.send();
+    }
+
+    // Handle XHR response.
+    function responseMethod() {
+      if (httpRequest.readyState === 4) {
+        console.log(httpRequest.responseText);
+      }
+    }
 })();
